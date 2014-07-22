29e28ab21932b5bb4bf2e223e2eba21364b6a615
scoin <greg.piccolo@gmail.com>
2014-07-21 20:41:43 -0400

    create blags index controller method

--- app/controllers/blags_controller.rb
+++ app/controllers/blags_controller.rb
@@ -1,6 +1,10 @@
 class BlagsController < ActionController::Base
 
 	def index
+		params[:n] ||= 1
+		post_view_count = 10
+		to_display = params[:n] * post_view_count
+		@blags = Post.all.limit(to_display).offset(to_display - post_view_count)
 	end
 
 	def show
